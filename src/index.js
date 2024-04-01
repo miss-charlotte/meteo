3f43e7e036bb33eed4e5bde7bc975c4326ea9674
Charlotte Akongo <charlotte.akongo@gmail.com>
2024-04-01 18:34:05 +0300

    added current time and date

--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
 function searchCity(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#current-city-details");
-    cityElement.innerHTML = searchInput.value
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city-details");
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener('submit', searchCity)
+searchFormElement.addEventListener("submit", searchCity);
+
+function formatDate(date) {
+  let day = date.getDay();
+  let dateDay = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "November",
+    "December",
+  ];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  let formattedDay = days[day];
+  let formattedMonth = months[month];
+
+  return `${formattedDay}, ${dateDay} ${formattedMonth}, ${year}  - ${hour}:${minutes}`;
+}
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
